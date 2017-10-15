7ec75c5f70ab0ad1454f6cffeca7a8c09d15f5df
Omar Shibli <omarshib@gmail.com>
2017-10-15 15:25:16 +0300

    cleanup

--- truffle.js
+++ truffle.js
@@ -1,13 +1,4 @@
 
-// imports
-const fs = require('fs');
-const hdkey = require('ethereumjs-wallet/hdkey');
-const ProviderEngine = require('web3-provider-engine');
-const WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-const Web3Subprovider = require('web3-provider-engine/subproviders/web3.js');
-const Web3 = require('web3');
-
-
 module.exports = {
   networks: {
     development: {
@@ -16,17 +7,4 @@ module.exports = {
       network_id: '*' // Match any network id
     }
   },
-  build: function(options, callback) {
-    const company =  options._[0].replace(/['']+/g, '');
-    const json = require('./build/contracts/CommerceBlockToken.json');
-    const contract = require('truffle-contract');
-    const CBToken = contract(json);
-
-    const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-    const MyContract = web3.eth.contract(CBToken.abi);
-
-    const contractData = MyContract.getData(company, {data: CBToken.unlinked_binary});
-    fs.writeFile('./CommerceBlockToken.bytecode', contractData);
-    fs.writeFile('./CommerceBlockToken.abi', contractData);
-   }
 };
